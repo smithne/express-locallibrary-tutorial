31f8532f5a0974961e88ebcde4d897c0561ef2ff
Nick Smith <smithne@uw.edu>
2019-09-08 15:56:47 -0700

    Add delete author controller methods & form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -113,13 +113,70 @@ exports.author_create_post = [
 ];
 
 // display author delete page (GET)
-exports.author_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author deletion page");
+exports.author_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      author: callback => {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books: callback => {
+        Book.find({ author: req.params.id }).exec(callback);
+      }
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        // no results
+        res.redirect("/catalog/authors");
+      }
+      // found author
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: results.author,
+        author_books: results.authors_books
+      });
+    }
+  );
 };
 
 // handle author deletion (POST)
-exports.author_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+exports.author_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      author: callback => {
+        Author.findById(req.body.authorid).exec(callback);
+      },
+      authors_books: callback => {
+        Book.find({ author: req.body.authorid }).exec(callback);
+      }
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      // success
+      if (results.authors_books.length > 0) {
+        // author still has books, can't delete
+        res.render("author_delete_get", {
+          title: "Delete Author",
+          author: results.author,
+          author_books: results.author_books
+        });
+        return;
+      } else {
+        // no books - safe to delete author
+        Author.findByIdAndRemove(req.body.authorid, err => {
+          if (err) {
+            return next(err);
+          }
+          // success, redirect to author list
+          res.redirect("/catalog/authors");
+        });
+      }
+    }
+  );
 };
 
 // display author update page (GET)
